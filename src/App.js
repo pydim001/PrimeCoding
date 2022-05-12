53f1bf79207645e079a30fe3e583fcb50322d821
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-05-12 12:07:28 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body className="App-body">
+        <div className='nav-bar'>
+          <div className='title'>Prime Coding</div>
+          <div className='options' id='videos'>Videos</div>
+          <div className='options'>Courses</div>
+          <div className='options'>Register</div>
+        </div>
+      </body >
+    </div >
   );
 }
 
