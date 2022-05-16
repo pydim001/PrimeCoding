f4ef6eddea9288e34097cab3999dc8fa6cb91173
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-05-16 00:04:57 -0500

    Working on fetch

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -2,10 +2,16 @@ import './Video.css';
 
 function Video() {
 
-    const getVideos = async () => {
-        const videos = await fetch('http://127.0.0.1:8000/videos/');
-        const response = await videos.json();
-        return response;
+    const getVideos = () => {
+        try {
+            const videos = fetch('http://127.0.0.1:8000/videos/')
+                .then(response => response.json()).then(jsoned => jsoned);
+            console.log(videos, 2);
+            return JSON.stringify(videos);
+        } catch (err) {
+            console.log(err);
+            return "An Error has Occurred";
+        }
     }
 
     return (
