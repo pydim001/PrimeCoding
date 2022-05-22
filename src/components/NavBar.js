aea3115fc5b4e8aa0f94a7a480f10907538ce297
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-05-22 00:47:13 -0500

    added hover css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
         <div className="App">
             <body className="App-body">
                 <div className='nav-bar'>
-                    <div className='title'><Link to='/' className='links'>Prime Coding</Link></div>
+                    <div className='title'><Link to='/' className='title-link' id="title">Prime Coding</Link></div>
                     <div className='option' id='about'><Link to='/about' className='links'>About</Link></div>
                     <div className='option'><Link to='/videos' className='links'>Videos</Link></div>
                     <div className='option'><Link to='/courses' className='links'>Courses</Link></div>
