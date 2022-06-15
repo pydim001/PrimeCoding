9cd2621a471f62dc537731830ebb86ff3e931d20
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-06-14 23:45:16 -0500

    about paging

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,13 @@ import './About.css';
 
 function About() {
     return (
-        <div>
+        <div className='about-div'>
             <div className='about-sec'>
                 <p>
-                    Hardworking college student pursuing a degree in Computer Science
+                    I am a hardworking college student pursuing a degree in Computer Science
                     and a Minor in Neuroscience at the University of Minnesota.
+                    My mission is to provide students and learners with the resources and knowledge of programming skills and Computer Science
+                    so that they will achieve their goals.
                 </p>
             </div>
         </div>
